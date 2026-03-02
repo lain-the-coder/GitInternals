5f51c5d2f879989fe1687d31d8a2733966d7c6b3
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-02 13:55:45 +0400

    Initial commit: Project setup with CLI structure

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitInternals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Internals Tool");
+            Console.WriteLine($"Arguments received: {args.Length}");
+        }
+    }
+}
