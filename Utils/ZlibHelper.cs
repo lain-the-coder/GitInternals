6d498fe1b79a127fccfff685b9c16fafed337fd2
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-02 22:22:47 +0400

    Created ZLibHelper util class skeleton

---
+++ Utils/ZlibHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace GitInternals.Utils
+{
+    public static class ZlibHelper
+    {
+        public static byte[] Decompress(byte[] compressedData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
