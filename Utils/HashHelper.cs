931b8b3db7b373213930744e39d924292a5a62c5
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-03 20:13:31 +0400

    Completed HashHelper function

--- Utils/HashHelper.cs
+++ Utils/HashHelper.cs
@@ -8,7 +8,13 @@ namespace GitInternals.Utils
     {
         public static string ComputeSHA1(byte[] data)
         {
-            throw new NotImplementedException();
+            // Calculate SHA-1 hash
+            byte[] hashBytes = SHA1.HashData(data);
+            // Input:  [98, 108, 111, 98, 32, 56, 55, ...]  (bytes)
+            // Output: [115, 102, 101, 83, 130, ...]        (20-byte hash)
+
+            // Convert to hex string
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
         }
     }
 }
