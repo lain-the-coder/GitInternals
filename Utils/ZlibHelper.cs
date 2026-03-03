8bd12240d3fe76c9567591bc1e45e7c3978e049d
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-03 15:41:45 +0400

    Implemented Decompress method in helper class

--- Utils/ZlibHelper.cs
+++ Utils/ZlibHelper.cs
@@ -9,7 +9,21 @@ namespace GitInternals.Utils
     {
         public static byte[] Decompress(byte[] compressedData)
         {
-            throw new NotImplementedException();
+            // Step 1: Skip first 2 bytes (header); convert bytes to stream
+            using (var compressedStream = new MemoryStream(compressedData, 2, compressedData.Length - 2))
+            {
+                // Step 2: Decompress with DeflateStream (straw); 
+                using (var deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+                {
+                    // Step 3: Write decompressed output; deflateStream needs to know where to decompress and copy to; use MemoryStream for output
+                    using (var decompressedStream = new MemoryStream())
+                    {
+                        deflateStream.CopyTo(decompressedStream);
+                        //Convert stream to byte array
+                        return decompressedStream.ToArray();
+                    }
+                }
+            }
         }
     }
 }
