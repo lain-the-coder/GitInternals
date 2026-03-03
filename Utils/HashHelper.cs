1152e5e9ced2c3a548b9c57c0806946a4b119fed
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-03-03 20:03:09 +0400

    Created shell for Stage 2: Computing Hash of a git object given it's content

---
+++ Utils/HashHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GitInternals.Utils
+{
+    public static class HashHelper
+    {
+        public static string ComputeSHA1(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
